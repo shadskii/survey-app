c214d8fcee0c626d155632b4de228a6c644285f6
shadskii <jake.k.hassel@gmail.com>
2018-08-24 11:19:03 -0400

    Feedback setup

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>send</v-icon>
+      </v-btn>
     </v-toolbar>
     <v-content>
-      <HelloWorld/>
+      <Feedback :questions="questions" comments/>
     </v-content>
     <v-footer app>
       <span>&copy; 2017</span>
@@ -13,16 +17,21 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Feedback from "./components/Feedback";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Feedback
   },
   data() {
     return {
-      title: "Give Us Your Feedback!"
+      title: "Give Us Your Feedback!",
+      questions: [
+        "If my instructor used these apps, it would enhance my understanding in the classroom.",
+        "The interactivity of the apps is more effective at teaching the topics than traditional teaching methods like textbooks and powerpoints.",
+        "The interactivity of the apps motivated me to want to use them."
+      ]
     };
   }
 };
